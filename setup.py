c8d04d977a9356d174dbfdec0c1f7c1118143a2f
Nestor Arocha <nestor@iwoca.co.uk>
2014-01-27 14:54:30 +0000

    added Nestor to authors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='opposite',
       version='1.0',
       description='False',
-      author='Mo Mirza',
+      author=['Mo Mirza', 'Nestor Arocha'],
       url='https://github.com/momirza/opposite',
       packages=find_packages(exclude=['test']),
      )
